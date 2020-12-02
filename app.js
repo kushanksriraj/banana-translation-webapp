b0b9e4960b4aa9307d74a0aa3040bbca7418f985
Kushank Sriraj <kushanksriraj@gmail.com>
2020-12-02 21:15:06 +0530

    ex07: call a mock server (from JS)

--- app.js
+++ app.js
@@ -1,12 +1,27 @@
-var buttonRef = document.querySelector("#btn-translate");
-var textInputRef = document.querySelector("#txt-input");
-var textOutputRef = document.querySelector("#text-output");
+// var buttonRef = document.querySelector("#btn-translate");
+// var textInputRef = document.querySelector("#txt-input");
+// var textOutputRef = document.querySelector("#text-output");
 
-function clickEventHandler() {
+// function clickEventHandler() {
     
-    var textInput = textInputRef.value;
-    console.log(textInput);  
-    textOutputRef.innerText = textInput;
-};
+//     var textInput = textInputRef.value;
+//     console.log(textInput);  
+//     textOutputRef.innerText = textInput;
+// };
 
-buttonRef.addEventListener("click", clickEventHandler);
+// buttonRef.addEventListener("click", clickEventHandler);
+
+
+// using arrow function
+fetch("https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json?text=neogcamp test").then(response => response.json()).then(response => console.log(response.contents.translated));
+
+// // using normal function
+// fetch("https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json?text=neogcamp test").then(function convert2json(response){
+//     return response.json();
+// }).then(function logOutput(response){
+//     console.log(response.contents.translated);
+// });
+
+
+
+
