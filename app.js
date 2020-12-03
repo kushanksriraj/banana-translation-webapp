fda17ade59d80a3bc77984c58057af9abf40fbf9
Kushank Sriraj <kushanksriraj@gmail.com>
2020-12-03 22:04:51 +0530

    style: added external css

--- app.js
+++ app.js
@@ -1,30 +1,33 @@
 var buttonRef = document.querySelector("#btn-translate");
 var textInputRef = document.querySelector("#txt-input");
-var textOutputRef = document.querySelector("#text-output");
-
-// const url = "https://lessonfourapi.tanaypratap.repl.co/tran-slate/yoda.json";
-
-const url = "https://api.funtranslations.com/translate/hodor.json";
+var textOutputRef = document.querySelector("#txt-output");
 
 
-function generateUrl(text){
+const url = "https://api.funtranslations.com/translate/minion.json";
+
+function generateUrl(text) {
+
     var temp = url + "?" + "text=" + text;
-    // console.log(encodeURI(temp));
     return encodeURI(temp);
 }
 
-function errorHandler(error){
+function errorHandler(error) {
+
     console.log("Some error occured!");
     console.log(error);
-    alert("Some error occured!");
+    alert("Some error occured! Please try after some time.")
 }
 
 function clickEventHandler() {
-    
+
     var textInput = textInputRef.value;
 
-    fetch(generateUrl(textInput)).then(response => response.json()).then(response => textOutputRef.innerText = response.contents.translated).catch(errorHandler);
-
+    fetch(generateUrl(textInput))
+        .then(response => response.json())
+        .then(json => {
+            textOutputRef.innerText = json.contents.translated;
+        })
+        .catch(errorHandler);
 }
 
 buttonRef.addEventListener("click", clickEventHandler);
